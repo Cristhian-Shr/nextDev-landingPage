90da02018e85e891d1962c20372e4e1a356b6b1d
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2024-10-24 21:35:20 -0300

    add componente provider, fontes, pasta componentes e configurações

--- src/app/_components/theme-provider.tsx
+++ src/app/_components/theme-provider.tsx
@@ -3,8 +3,10 @@
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
-function ThemeProvider ({ children, ...props} : ThemeProviderProps) {
+export function ThemeProvider ({ children, ...props} : ThemeProviderProps) {
     return(
-        
+        <NextThemeProvider {...props}>
+            {children}
+        </NextThemeProvider>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <section id="intro">
+      <section id="intro" className="space-y-6 py-8 md:py-12 lg:py-32">
+        <div className="container flex max-w-[64rem] flex-col items-center text-center gap-4">
+          <span>Faça o seu projeto com a melhor tech do momento</span>
+        </div>
         <h1 className="text-primary">the next dev</h1>
       </section>
     </main>
