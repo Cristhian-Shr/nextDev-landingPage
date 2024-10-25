38be3f119f70c9f8caf102b4d05f0abc8e78efd5
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2024-10-25 20:51:31 -0300

    add sessão de FAQ e Caracteristicas

--- src/app/_components/faq-section.tsx
+++ src/app/_components/faq-section.tsx
@@ -9,7 +9,7 @@ function FaqSection() {
   return (
     <section
       id="faq"
-      className="bg-slate-50 dark:bg-transparent container space-y-6 py-8 md:py-12 lg:py-24"
+      className="bg-slate-50 dark:bg-transparent container space-y-6 py-8 md:py-12 lg:py-16"
     >
       <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
         <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
